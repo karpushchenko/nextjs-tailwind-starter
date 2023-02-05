b7d2f50530029141c54ebe0513fc665c2ca266fb
Ivan Karpushchenko <karpushchenko@mail.ru>
2023-02-05 22:55:08 +0400

    fix: root prop types set to any

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line react/prop-types
+import PropTypes from 'prop-types';
 import '../styles/main.css';
 
 const RootLayout = ({ children }) => (
@@ -7,5 +7,10 @@ const RootLayout = ({ children }) => (
     <body>{children}</body>
   </html>
 );
+
+// TODO: set ptoper propType for children
+RootLayout.propTypes = {
+  children: PropTypes.any,
+};
 
 export default RootLayout;
